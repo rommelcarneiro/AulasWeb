b84d437f880ddcae92ff426be1e2734df22d430e
Rommel Carneiro <rommelcarneiro@gmail.com>
2017-04-12 18:08:49 -0300

    Adicionando Lab-Prism

---
+++ Lab-Prism/code.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/prism.css" />
+</head>
+<body>
+    
+    <pre><code class="language-http">GET / HTTP/1.1
+Host: www.webdevbr.com.br<br/>
+User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0
+Referer: http://www.rexswain.com/httpview.html
+Connection: close
+</code></pre>
+
+   
+   
+    
+    <script src="js/prism.js"></script>
+</body>
+</html>
