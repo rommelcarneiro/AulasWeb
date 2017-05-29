065cf0d445e722c7c2fd573027b4f8285d77fac4
Rommel Carneiro <rommelcarneiro@gmail.com>
2017-05-29 01:56:03 -0300

    Retirada de chaves do Lab-OAuth/config.php

--- Lab-OAuth/config.php
+++ Lab-OAuth/config.php
@@ -7,16 +7,16 @@ $CALLBACK_URL = "http://localhost/AulasWeb/Lab-OAuth/OAuthCallback.php";
 $GOOGLE_API_AUTH_URL = "https://accounts.google.com/o/oauth2/auth";
 $GOOGLE_API_TOKEN_URL = "https://accounts.google.com/o/oauth2/token";
 $GOOGLE_API_RESOURCE_URL = "https://www.googleapis.com/oauth2/v1/userinfo";
-$GOOGLE_CLIENT_ID = "6199248294-srq3i9ujbdr0ks0ckft5qsngrvmo4t78.apps.googleusercontent.com";
-$GOOGLE_CLIENT_SECRET_KEY = "dotW00EHSR60Zos_Zuc5AsaN";
+$GOOGLE_CLIENT_ID = "COLOQUE AQUI SEU CLIENT_ID";
+$GOOGLE_CLIENT_SECRET_KEY = "COLOQUE AQUI SEU SECRET_KEY";
 $GOOGLE_SCOPE = "https://www.googleapis.com/auth/plus.me";
     
 // Configuração da API do Facebook
 $FACEBOOK_API_AUTH_URL = "https://www.facebook.com/v2.8/dialog/oauth";
 $FACEBOOK_API_TOKEN_URL = "https://graph.facebook.com/v2.8/oauth/access_token";
 $FACEBOOK_API_RESOURCE_URL = "https://graph.facebook.com/v2.8/me";
-$FACEBOOK_CLIENT_ID = "244927775973840";
-$FACEBOOK_CLIENT_SECRET_KEY = "1e273f44b2197c0ced0aca856db5659d";
-$FACEBOOK_SCOPE = "public_profile, email";
+$FACEBOOK_CLIENT_ID = "COLOQUE AQUI SEU CLIENT_ID";
+$FACEBOOK_CLIENT_SECRET_KEY = "COLOQUE AQUI SEU SECRET_KEY";
+$FACEBOOK_SCOPE = "public_profile, email";    
 
 ?>
